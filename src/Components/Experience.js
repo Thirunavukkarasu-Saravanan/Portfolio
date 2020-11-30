699ae4912549fc398cf691a62cb69d748f2c8939
Saravanan Thirunavukkarasu <proto@gmail.com>
2020-11-29 21:27:51 -0600

    Added Component for experience, need to work on replicating the same for an array of inputs

--- src/Components/Experience.js
+++ src/Components/Experience.js
@@ -1,10 +1,7 @@
 import React from "react";
 import cont from "../Images/cont.png";
-import { Accordion } from "react-bootstrap";
-import { Card } from "react-bootstrap";
-import { Container } from "react-bootstrap";
-import { Row } from "react-bootstrap";
-import { Col } from "react-bootstrap";
+import { Accordion, Card, Container, Row, Col, Button } from "react-bootstrap";
+
 import entries from "../Data/data-entry";
 
 function Experience() {
@@ -13,34 +10,46 @@ function Experience() {
   let reference1 = entries.Tags[2].Experience[0].ReferenceName;
   let referencEmail1 = entries.Tags[2].Experience[0].ReferenceEmail;
   let referencContact1 = entries.Tags[2].Experience[0].ReferencePhone;
+  let duration1 = entries.Tags[2].Experience[0].Duration;
+  let companyName1 = entries.Tags[2].Experience[0].Company;
   return (
     <Container fluid>
       <Row>
-        <Col sm={8}>
-          <Accordion>
-            <Card>
-              <Accordion.Toggle as={Card.Header} eventKey="0">
-                {role1}
-              </Accordion.Toggle>
-              <Accordion.Collapse eventKey="0">
-                <Card.Body>{skills1}</Card.Body>
-              </Accordion.Collapse>
-            </Card>
-            <Card>
-              <Accordion.Toggle as={Card.Header} eventKey="1">
-                Reference
-              </Accordion.Toggle>
-              <Accordion.Collapse eventKey="1">
-                <Card.Body>{reference1}</Card.Body>
-              </Accordion.Collapse>
-              <Accordion.Collapse eventKey="1">
-                <Card.Body>{referencEmail1}</Card.Body>
-              </Accordion.Collapse>
-              <Accordion.Collapse eventKey="1">
-                <Card.Body>{referencContact1}</Card.Body>
-              </Accordion.Collapse>
-            </Card>
-          </Accordion>
+        <Col sm={3}></Col>
+        <Col sm={6}>
+          <Card className="text-center">
+            <Card.Header as="h5">{role1}</Card.Header>
+            <Card.Body>
+              <Card.Text>{companyName1}</Card.Text>
+              <Col sm={13}>
+                <Accordion>
+                  <Card>
+                    <Accordion.Toggle as={Card.Header} eventKey="0">
+                      Duties
+                    </Accordion.Toggle>
+                    <Accordion.Collapse eventKey="0">
+                      <Card.Body>{skills1}</Card.Body>
+                    </Accordion.Collapse>
+                  </Card>
+                  <Card>
+                    <Accordion.Toggle as={Card.Header} eventKey="1">
+                      Reference
+                    </Accordion.Toggle>
+                    <Accordion.Collapse eventKey="1">
+                      <Card.Body>{reference1}</Card.Body>
+                    </Accordion.Collapse>
+                    <Accordion.Collapse eventKey="1">
+                      <Card.Body>{referencEmail1}</Card.Body>
+                    </Accordion.Collapse>
+                    <Accordion.Collapse eventKey="1">
+                      <Card.Body>{referencContact1}</Card.Body>
+                    </Accordion.Collapse>
+                  </Card>
+                </Accordion>
+              </Col>
+            </Card.Body>
+            <Card.Footer className="text-muted">{duration1}</Card.Footer>
+          </Card>
         </Col>
       </Row>
     </Container>
